32f024be547c5946f5b76abc61a6ea6ff2a95297
harsha <harshavardhanpillimitla143@gmail.com>
2020-06-21 22:51:21 +0530

    'initial'

--- ft/settings.py
+++ ft/settings.py
@@ -120,5 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
+
 import dj_database_url
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
